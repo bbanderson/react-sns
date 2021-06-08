b824794e456c479dbd60d602de74d5e1e418c7b9
bbanderson <tombyun@naver.com>
2021-06-08 15:16:59 +0900

    Separate reducer

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -5,7 +5,7 @@ import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
 import useInput from '../hooks/useInput';
 import { useDispatch } from 'react-redux';
-import { loginAction } from '../reducers';
+import { loginAction } from '../reducers/user';
 
 const FormWrapper = styled(Form)`
   padding: 10px;
--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,69 +1,34 @@
 // reducer는 함수다! 개발자가 원하는 수정사항에 대한 실제 로직을 이 안에 모두 구현하자!
 import { HYDRATE } from 'next-redux-wrapper';
-const initialState = {
-  user: {
-    isLoggedIn: false,
-    user: null,
-    signUpData: {},
-    loginData: {},
-  },
-  posts: {
-    mainPosts: [],
-  },
-};
-
-export const loginAction = (data) => {
-  return {
-    type: 'LOG_IN',
-    data,
-  };
-};
-
-export const logoutAction = () => {
-  return {
-    type: 'LOG_OUT',
-  };
-};
+import { combineReducers } from 'redux';
+import user from './user';
+import post from './post';
 
 const changeNickName = {
   type: 'CHANGE_NICKNAME',
   data: 'bbanderson',
 };
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      console.log('HYDRATE', action);
-      return {
-        ...state,
-        ...action.payload,
-      };
-    case 'CHANGE_NICKNAME':
-      return {
-        ...state,
-        name: action.name,
-      };
-    case 'LOG_IN':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: true,
-          user: action.data,
-        },
-      };
-    case 'LOG_OUT':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          isLoggedIn: false,
-          user: null,
-        },
-      };
-    default:
-      return state;
-  }
-};
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE: // HYDRATE: SSR을 위함. 사실상 SSR을 위해 index.js가 필요.
+        console.log('HYDRATE', action);
+        return {
+          ...state,
+          ...action.payload,
+        };
+      case 'CHANGE_NICKNAME':
+        return {
+          ...state,
+          name: action.name,
+        };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default rootReducer;
